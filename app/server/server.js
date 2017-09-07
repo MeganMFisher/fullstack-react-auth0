1d85684ffbc8e4c7b3f1d263150a3e66ca48ef9c
MeganMFisher <MeganMichelleFisher@gmail.com>
2017-09-07 07:32:12 -0600

    switched stuff around

--- app/server/server.js
+++ app/server/server.js
@@ -22,7 +22,8 @@ app.use(passport.session()); //If your application uses persistent login session
 
 massive(process.env.CONNECTIONSTRING).then( db => {
     app.set('db', db);
-  }) //
+  }) 
+ const db = app.get('db')
 
 
 passport.use(new Auth0Strategy({
@@ -36,50 +37,47 @@ passport.use(new Auth0Strategy({
     // extraParams.id_token has the JSON Web Token
     // profile has all the information from the user
     //A Refresh Token is a special kind of token that can be used to obtain a renewed access token —that allows accessing a protected resource— at any time. You can request new access tokens until the refresh token is blacklisted. Refresh tokens must be stored securely by an application because they essentially allow a user to remain authenticated forever.
-
-      const db = app.get('db') //setting app database connection to the db to a variable that is easier to use. On app there is a get property we are trying to access which has our db on it. 
-
-      // console.log(app.get('db')) //Will show you your queries and everything else regarding your db.
-
-      console.log(profile.identities[0].user_id) //To see the individual user id. //Will get you the id that will look like this: 1121...... Doesn't have google-0auth2 in front of it. 
-
-      console.log(profile.id) //Will get you the id but it will look like this: google-oauth2|1121.....
+      const db = app.get('db')
     
       db.find_user([ profile.identities[0].user_id ])
       .then( user => {
        if ( user[0] ) {
-         return done( null, { id: user[0].id } );
+         return done( null, user );
           //Sends the user_id given to us by Auth0 to our find_user query which selects the user with that same id if they are already in the db. If the user is there and the sql query returns it then return done(). 
-
-
-       } else {
-    
+       } else {    
          db.create_user([profile.displayName, profile.emails[0].value, profile.picture, profile.identities[0].user_id])
          .then( user => {
-            return done( null, { id: user[0].id } );
+            return done( null, user[0] ); // GOES TO SERIALIZE USER //Your endpoints need to know who the user is so done passes it along for you when you enter it in as the second parameter. 
          })
-    
        }
       })
-
-    
 }));
 
 //Passport will maintain persistent login sessions. In order for persistent sessions to work, the authenticated user must be serialized to the session, and deserialized when subsequent requests are made.
 
+
+//THIS IS INVOKED ONE TIME TO SET THINGS UP
 passport.serializeUser(function(user, done) {
+  // console.log(user)
+  //Things you might do here :
+   //Serialize just the id, get other information to add to session, 
   done(null, user);
-});
+}); ////SerializeUser is invoked one time to set things up on session. It takes in a function and the parameters are whatever we passed into done(), so the user and then done. Its a sandbox where we can do whatever we want to here. for now we can get the object and pass it into done again. We could do a lot of things here. To make it simple just get it and pass it into done again. *Puts it on session. We can put anything we want on it. 
 
 //Passport does not impose any restrictions on how your user records are stored. Instead, you provide functions to Passport which implements the necessary serialization and deserialization logic. In a typical application, this will be as simple as serializing the user, and finding the user by ID when deserializing.
 
+
+//USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
 passport.deserializeUser(function(user, done) {
-  app.get('db').find_session_user([user.id])
+  console.log(user)
+  app.get('db').find_session_user([user[0].id]) //<== The id we gave them in our db with the serial primary key.
   .then( user => {
-    console.log(user)
     return done(null, user[0]);
   })
-});
+});//We could just pass the user on so it gets onto req.user but this time we are going to make a db query to find the session user with the id which has the displayName, email, picture, id of the user and we will put all of that onto the req.user. *Takes it off session and puts it on req.user. 
+
+
+// <!-- req.session.passport.user and on req.user --> Its in both places. 
 
 //if you just do user and not user[0] you receive the following: 
 //[ anonymous {
@@ -89,14 +87,15 @@ passport.deserializeUser(function(user, done) {
   // img: 'url',
   // auth_id: 'id' } ]
 
-// Passport provides an authenticate() function, which is used as route middleware to authenticate requests.
 
-app.get('/auth', passport.authenticate('auth0')); //If this function gets called, authentication was successful. `req.user` contains the authenticated user.
 
-app.get('/auth/callback', passport.authenticate('auth0', {
+
+app.get('/auth', passport.authenticate('auth0')); // Login button hits this endpoint to start the process. Passport provides an authenticate() function, which is used as route middleware to authenticate requests. If this function gets called, authentication was successful. `req.user` contains the authenticated user.
+
+app.get('/auth/callback', passport.authenticate('auth0', { //Auth0 is going to hit this callback endpoint.
   successRedirect: 'http://localhost:3000/#/private',
   failureRedirect: 'http://localhost:3000/#/'
-})) //A redirect is commonly issued after authenticating a request. In this case, the redirect options override the default behavior. Upon successful authentication, the user will be redirected to the Private page with their account information. If authentication fails, the user will be redirected back to the login page for another attempt.
+})) //A redirect is commonly issued after authenticating a request. Upon successful authentication, the user will be redirected to the Private page with their account information. If authentication fails, the user will be redirected back to the login page for another attempt.
 
 app.get('/auth/me', (req, res, next) => {
   if (!req.user) {
@@ -129,3 +128,4 @@ app.listen(PORT, () => {
 // When Passport authenticates a request, it parses the credentials contained in the request. It then invokes the verify callback with those credentials as arguments. If the credentials are valid, the verify callback invokes done to supply Passport with the user that authenticated.
 
 // If the credentials are not valid (for example, if the password is incorrect), done should be invoked with false instead of a user to indicate an authentication failure.
+
